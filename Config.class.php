d2148208f5972ef9bcbe68d6217734e2a0678e89
Gerald Todd <beingforthebenefit@gmail.com>
2022-08-10 21:29:57 -0700

    Write 'load()' method and supporting 'casteType()' method in Config class.

--- Config.class.php
+++ Config.class.php
@@ -22,6 +22,47 @@ class Config {
 
     // \Config::load :: void -> bool
     public static function load() {
+        $configFile = fopen(self::FILENAME, 'r');
 
+        while (!feof($configFile)) {
+            $line = fgets($configFile);
+
+            // If the line does not begin with a '#' and contains '=' somewhere not at the beginning
+            if (
+                $line[0] != '#'
+                && str_contains($line, '=')
+                && $line[0] != '='
+            ) {
+
+                // explode on first '=' only
+                $parts = explode('=', $line, 2);
+
+                $parts[0] = trim($parts[0]);
+
+                $parts[1] = self::castType(
+                    trim($parts[1])
+                );
+
+                self::$config[] = $parts;
+            }
+        }
+
+        return count(self::$config) != 0;
+    }
+
+    // \Config::castType :: a -> a
+    public static function castType($data) {
+        $value = null;
+
+        if (array_key_exists(strtolower($data), self::BOOL_MAP)) {
+            $value = self::BOOL_MAP[strtolower($data)];
+        } elseif (is_numeric($data)) {
+            // If string is entirely numerical (no decimal), cast to integer. Else, cast to float.
+            $value = ctype_digit($data) ? (int)$data : (float)$data;
+        } else {
+            $value = $data;
+        }
+
+        return $value;
     }
 }
