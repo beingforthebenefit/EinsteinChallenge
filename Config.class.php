bd0aa39637994b99d9f7352479730411f58a450c
Gerald Todd <beingforthebenefit@gmail.com>
2022-08-10 21:28:10 -0700

    Add Config class constants and a function signature.

--- Config.class.php
+++ Config.class.php
@@ -3,7 +3,24 @@
 
 class Config {
 
-    // \Config::load :: void -> void
+    // \Config::FILENAME :: string
+    public const FILENAME = '.config';
+
+    // \Config::BOOL_MAP :: [a -> bool]
+    // Requires PHP version 5.6 or greater
+    public const BOOL_MAP = [
+        'true' => true,
+        'false' => false,
+        'yes' => true,
+        'no' => false,
+        'on' => true,
+        'off' => false
+    ];
+
+    // \Config::FILENAME :: [string -> int|float|str|bool]
+    public static $config = [];
+
+    // \Config::load :: void -> bool
     public static function load() {
 
     }
